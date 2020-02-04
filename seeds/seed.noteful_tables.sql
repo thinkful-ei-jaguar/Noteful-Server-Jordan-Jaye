9762c3d5c62915e7be47019fa50b09ede3f67d13
Jordan Castillo <jordanxcallaway@gmail.com>
2020-02-04 12:11:02 -0800

    migrations and seed files for noteful database

--- seeds/seed.noteful_tables.sql
+++ seeds/seed.noteful_tables.sql
@@ -1,11 +1,11 @@
 INSERT INTO folders (id, folder_name)
 VALUES 
-  ('b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1', 'Important'),
-  ('b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1', 'Super'),
-  ('b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1', 'Spangley');
+  (1, 'Important'),
+  (2, 'Super'),
+  (3, 'Spangley');
 
 INSERT INTO notes (id, note_name, modified, folder_id, content)
 VALUES
-  ('cbc787a0-ffaf-11e8-8eb2-f2801f1b9fd1', 'Dogs', '2019-01-03T00:00:00.000Z', 'b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1', 'Ex autem nemo quisquam voluptas consequuntur et necessitatibus minima velit. Consequatur quia quis tempora minima. Aut qui dolor et dignissimos ut repellat quas ad.'),
-  ('d26e0034-ffaf-11e8-8eb2-f2801f1b9fd1', 'Cats', '2018-08-15T23:00:00.000Z', 'b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1', 'Ex autem nemo quisquam voluptas consequuntur et necessitatibus minima velit. Consequatur quia quis tempora minima. Aut qui dolor et dignissimos ut repellat quas ad.'),
-  ('d26e01a6-ffaf-11e8-8eb2-f2801f1b9fd1', 'Pigs', '2018-03-01T00:00:00.000Z', 'b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1', 'Ex autem nemo quisquam voluptas consequuntur et necessitatibus minima velit. Consequatur quia quis tempora minima. Aut qui dolor et dignissimos ut repellat quas ad.');
+  (01, 'Dogs', '2019-01-03T00:00:00.000Z', 1, 'Ex autem nemo quisquam voluptas consequuntur et necessitatibus minima velit. Consequatur quia quis tempora minima. Aut qui dolor et dignissimos ut repellat quas ad.'),
+  (02, 'Cats', '2018-08-15T23:00:00.000Z', 2, 'Ex autem nemo quisquam voluptas consequuntur et necessitatibus minima velit. Consequatur quia quis tempora minima. Aut qui dolor et dignissimos ut repellat quas ad.'),
+  (03, 'Pigs', '2018-03-01T00:00:00.000Z', 2, 'Ex autem nemo quisquam voluptas consequuntur et necessitatibus minima velit. Consequatur quia quis tempora minima. Aut qui dolor et dignissimos ut repellat quas ad.');
