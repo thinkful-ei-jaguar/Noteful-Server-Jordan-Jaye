36b216aab5a9384dc7b537518e8ffe5a9c2eb8c1
Jaye Anne Laguardia <jayelaguardia@gmail.com>
2020-02-05 11:20:54 -0800

    minor changes for deploy

--- postgrator.config.js
+++ postgrator.config.js
@@ -1,3 +1,3 @@
 /* eslint-disable semi */ require('dotenv').config()
 
-module.exports = { 'migrationsDirectory': 'migrations', 'driver': 'pg', 'connectionString': (process.env.NODE_ENV === 'test') ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL, }
+module.exports = { 'migrationsDirectory': 'migrations', 'driver': 'pg', 'connectionString': (process.env.NODE_ENV === 'test') ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL, 'ssl': !!process.env.SSL,}
