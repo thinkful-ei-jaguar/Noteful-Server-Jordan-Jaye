75aa4b7a48ac36e824a3bf28c93be59aa9785663
Jaye Anne Laguardia <jayelaguardia@gmail.com>
2020-02-05 09:37:27 -0800

    final touches

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
 const knex = require('knex');
 const app = require('./app');
-const { DB_URL, PORT } = require('./config');
+const { DATABASE_URL, PORT } = require('./config');
 
 
-const db = knex ({ client: 'pg', connection: DB_URL });
+const db = knex ({ client: 'pg', connection: DATABASE_URL });
 
 app.set('db', db);
 
-app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}...`));
+app.listen(PORT, () => console.log('Server is listening...'));
