0ec1323cb6fc94e88e9709293aa72359296692ef
Jordan Castillo <jordanxcallaway@gmail.com>
2020-02-05 13:20:50 -0800

    deployed changes

--- postgrator.config.js
+++ postgrator.config.js
@@ -1,3 +1,11 @@
-/* eslint-disable semi */ require('dotenv').config()
+/* eslint-disable semi */ 
+require('dotenv').config()
 
-module.exports = { 'migrationsDirectory': 'migrations', 'driver': 'pg', 'connectionString': (process.env.NODE_ENV === 'test') ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL, 'ssl': !!process.env.SSL,}
+module.exports = { 
+  'migrationsDirectory': 'migrations', 
+  'driver': 'pg', 
+  'connectionString': (process.env.NODE_ENV === 'test') 
+    ? process.env.TEST_DATABASE_URL 
+    : process.env.DATABASE_URL, 
+  'ssl': !!process.env.SSL,
+}
